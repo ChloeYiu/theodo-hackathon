79a32b7a8422628c6330b6d7a6f37f70ce8c8a7c
benkw26 <bensonxkwok@gmail.com>
2023-01-20 16:01:45 +0000

    Remove unecessary pages

--- src/components/molecules/Header/index.js
+++
@@ -1,9 +0,0 @@
-import "./Header.css";
-
-export default function Header() {
-  return (
-    <div className="header">
-      <h1> This is a header </h1>
-    </div>
-  );
-}
--- src/pages/theodo/index.js
+++
@@ -1,9 +0,0 @@
-const Theodo = () => {
-  return (
-    <div>
-      <h1>Theodo Page</h1>
-    </div>
-  );
-};
-
-export default Theodo;
